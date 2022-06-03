4501f21238c81eb0657ffe5699d00d4046cc2e5d
Fadel <fadel.seydou@epfl.ch>
2022-06-03 17:20:06 +0200

    untracking file

--- DIS_21-22_hwk2_material/part2/part2_2/webots/controllers/pso_sup/build/release/pso.d
+++
@@ -1,5 +0,0 @@
-build/release/pso.o: pso.c \
- C:\Program\ Files\Webots/include/controller/c/webots/supervisor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/contact_point.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h pso.h
--- DIS_21-22_hwk2_material/part2/part2_2/webots/controllers/pso_sup/build/release/pso_sup.d
+++
@@ -1,8 +0,0 @@
-build/release/pso_sup.o: pso_sup.c pso.h \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/emitter.h \
- C:\Program\ Files\Webots/include/controller/c/webots/receiver.h \
- C:\Program\ Files\Webots/include/controller/c/webots/supervisor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/contact_point.h
