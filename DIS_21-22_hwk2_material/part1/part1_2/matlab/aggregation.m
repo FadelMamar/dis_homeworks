809a460b53aceb4d84c25ddf688f1747d4792dae
Fadel <fadel.seydou@epfl.ch>
2022-06-05 15:22:08 +0200

    update

--- DIS_21-22_hwk2_material/part1/part1_2/matlab/aggregation.m
+++ DIS_21-22_hwk2_material/part1/part1_2/matlab/aggregation.m
@@ -1,21 +1,21 @@
-% Usual cleaning
+%% Usual cleaning
 clc
 close all
 clear all
 
 % Initialize parameters
 Nr = 5; % Total number of robots
-Ns = 5; % Total number of seeds
+Ns = 10; % Total number of seeds
 Wsf0 = Nr; % Number of robots in search-free state
 Wsl0 = 0; % Number of robots in search-loaded state
 Wr0 = 0; % Number of robots in releasing state
 Wg0 = 0; % Number of robots in gripping state
 Wof0 = 0; % Number of robots in obstacle avoidance-free state
 Wol0 = 0; % Number of robots in obstacle avoidance-floaded state
-k_sim = 1000; % Total simulation time
+k_sim = 10080; % Total simulation time
 vr = 0.08; %average robot speed, m/s
 rs = 0.1; %detection radius of the distance sensor
-ds = 0.06; %detection distance of the sensor
+ds = 2*rs; %detection distance of the sensor
 da = 1; % edge of the arena
 Aa = da^2; % Area of the arena, m^2
 Ts = 1; % Sample time, s
@@ -23,6 +23,8 @@ Trg = 2; % Releasing and gripping delay, s
 Toa = 3; % Obstacle avoidance delay, s
 alpha_inc = 60; % Construction angle
 alpha_dec = 60; % Destruction angle
+Td_max = max(Trg,Toa)+1; % Simulation start
+p_thr_sbc = 0.1; % Probability threshold for size of the biggest cluster
 
 % Allocate variables
 Wsf = zeros(k_sim,1);
@@ -32,13 +34,10 @@ Wg = zeros(k_sim,1);
 Wof = zeros(k_sim,1);
 Wol = zeros(k_sim,1);
 Ncn = zeros(k_sim,Ns+1,1); % Number of clusters of size n (max Ns)
-Nc = zeros(k_sim,1); % Number of clusters NC
-Nac = zeros(k_sim,1); % Average cluster size ACS
-NC = zeros(k_sim,1);
-ACS = zeros(k_sim,1);
-SBC = zeros(k_sim,1);
-
-Td_max = max(Trg,Toa)+1;
+NC = zeros(k_sim,1); % Number of clusters 
+ACS = zeros(k_sim,1); % Average size of the clusters
+SBC = zeros(k_sim,1); % Size of the biggest cluster
+SC = zeros(k_sim,1); % Sanity check
 
 % Initialize variables
 Wsf(Td_max) = Wsf0;
@@ -48,7 +47,6 @@ Wg(Td_max) = Wg0;
 Wol(Td_max) = Wol0;
 Wof(Td_max) = Wof0;
 Ncn(1:Td_max,1) = Ns;
-Nac(1:Td_max) = 1;
 NC(Td_max) = 0;
 ACS(Td_max) = 0;
 SBC(Td_max) = 0;
@@ -59,8 +57,7 @@ for k = Td_max:k_sim
                     
 end
 
-%%
-% Plot number of robots in each state
+%% Plot number of robots in each state
 figure
 plot(Wsf,'LineWidth',2)
 hold on
@@ -91,6 +88,3 @@ plot(SBC,'LineWidth',2)
 legend('NC','ACS','SBC');
 xlabel('Discrete time (k)');
 ylabel('Number');
-
-
-
