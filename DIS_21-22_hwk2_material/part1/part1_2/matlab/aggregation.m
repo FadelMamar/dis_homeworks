27b88cd4dcf1820db3ecaa26ef0885f3606e9fcd
Fadel <fadel.seydou@epfl.ch>
2022-06-06 17:25:03 +0200

    update

--- DIS_21-22_hwk2_material/part1/part1_2/matlab/aggregation.m
+++ DIS_21-22_hwk2_material/part1/part1_2/matlab/aggregation.m
@@ -21,8 +21,8 @@ Aa = da^2; % Area of the arena, m^2
 Ts = 1; % Sample time, s
 Trg = 2; % Releasing and gripping delay, s
 Toa = 3; % Obstacle avoidance delay, s
-alpha_inc =@(n) (n>1)*60 + (n<2)*1; % Construction angle
-alpha_dec =@(n) (n>1)*60 + (n<2)*1; % Destruction angle
+alpha_inc =@(n) (n>=2)*60 + (n==1)*180; % Construction angle
+alpha_dec =@(n) (n>=2)*60 + (n==1)*180; % Destruction angle
 Td_max = max(Trg,Toa)+1; % Simulation start
 p_thr_sbc = 0.1; % Probability threshold for size of the biggest cluster
 dr=0.07;
@@ -39,11 +39,11 @@ NC = zeros(k_sim,1); % Number of clusters
 ACS = zeros(k_sim,1); % Average size of the clusters
 SBC = zeros(k_sim,1); % Size of the biggest cluster
 SC = zeros(k_sim,1); % Sanity check
-p_1 = zeros(k_sim,1);
-p_2 = zeros(k_sim,1);
+p_dec =@(k,n) Ts*(alpha_dec(n)/180).*(Ncn(k,n)*vr*ds/Aa) ;
+p_inc =@(k,n) Ts*(alpha_inc(n)/180).*(Ncn(k,n)*vr*ds/Aa) ;
+
 p_3 = (vr*Ts/(da-2*rs)) + (Nr-1)*(2*rs+dr)*vr/Aa;
 p_4 = p_3 + Ns*(2*rs+seed_diameter)*vr/Aa;
-
 
 % Initialize variables
 Wsf(Td_max) = Wsf0;
@@ -58,31 +58,35 @@ ACS(Td_max) = 0;
 SBC(Td_max) = 0;
 
 % Perform the simulation
-
-
-for k = Td_max:k_sim-1
+for k = Td_max:k_sim
     
-    %--
-    p_dec =@(k,n) Ts*(alpha_dec(n)/180).*(Ncn(k,n)*vr*ds/Aa) ;
-    p_inc =@(k,n) Ts*(alpha_inc(n)/180).*(Ncn(k,n)*vr*ds/Aa) ;
-    
-    for n=2:Ns 
-        Ncn(k+1,n) = Ncn(k,n) + Wsf(k-Trg)*(p_dec(k-Trg,n+1)*Ncn(k-Trg,n+1) - p_dec(k-Trg,n)*Ncn(k-Trg,n)) + ...
-                     Wsl(k-Trg)*(p_inc(k-Trg,n-1)*Ncn(k-Trg,n-1) - p_inc(k-Trg,n)*Ncn(k-Trg,n));
-                     
+    Ncn(k+1,1) = Ncn(k,1) + Wsf(k-Trg)*(p_dec(k-Trg,1+1)*Ncn(k-Trg,1+1) - p_dec(k-Trg,1)*Ncn(k-Trg,1)) + ...
+               Wsl(k-Trg)*(0.0 - p_inc(k-Trg,1)*Ncn(k-Trg,1)); 
+    for n=2:Ns-1 
+           Ncn(k+1,n) = Ncn(k,n) + Wsf(k-Trg)*(p_dec(k-Trg,n+1)*Ncn(k-Trg,n+1) - p_dec(k-Trg,n)*Ncn(k-Trg,n)) + ...
+               Wsl(k-Trg)*(p_inc(k-Trg,n-1)*Ncn(k-Trg,n-1) - p_inc(k-Trg,n)*Ncn(k-Trg,n));                 
     end
     
-    p_1(k) = dot(p_dec(k,(1:Ns+1)),Ncn(k,:,1));
-    p_2(k) = dot(p_inc(k,(1:Ns+1)),Ncn(k,:,1));
+    p_1 =@(k) dot(p_dec(k,(1:Ns)),Ncn(k,1:Ns,1));
+    p_2 =@(k) dot(p_inc(k,(1:Ns)),Ncn(k,1:Ns,1));
     
-    %---
-    Wof(k+1) = Wof(k) + p_3*Wsf(k)    - p_3*Wsf(k-Toa);
-    Wol(k+1) = Wol(k) + p_4*Wsl(k)    - p_4*Wsl(k-Toa);
-    Wr(k+1)  = Wr(k)  + p_2(k)*Wsl(k) - p_2(k-Trg)*Wsl(k-Trg);
-    Wg(k+1)  = Wg(k)  + p_1(k)*Wsf(k) - p_1(k-Trg)*Wsf(k-Trg);
-    Wsl(k+1) = Wsl(k)*(1-p_2(k)-p_4) + p_1(k-Trg)*Wsf(k-Trg) + p_4*Wsl(k-Toa);
-    Wsf(k+1) = Wsf(k)*(1-p_1(k)-p_3) + p_2(k-Trg)*Wsl(k-Trg) + p_3*Wsf(k-Toa); 
+    %--
+    Wof(k+1) = Wof(k) + p_3*Wsf(k)    - p_3*Wsf(k-Toa)*(k-Toa>0);
+    Wol(k+1) = Wol(k) + p_4*Wsl(k)    - p_4*Wsl(k-Toa)*(k-Toa>0);
+    Wr(k+1)  = Wr(k)  + p_2(k)*Wsl(k) - p_2(k-Trg)*Wsl(k-Trg)*(k-Trg>0);
+    Wg(k+1)  = Wg(k)  + p_1(k)*Wsf(k) - p_1(k-Trg)*Wsf(k-Trg)*(k-Trg>0);
+    Wsl(k+1) = Wsl(k)*(1-p_2(k)-p_4) + p_1(k-Trg)*Wsf(k-Trg)*(k-Trg>0)+ p_4*Wsl(k-Toa)*(k-Toa>0);
+    Wsf(k+1) = Wsf(k)*(1-p_1(k)-p_3) + p_2(k-Trg)*Wsl(k-Trg)*(k-Trg>0) + p_3*Wsf(k-Toa)*(k-Toa>0);
     
+    disp('-------'); disp(k);
+    [Wof(k), Wol(k), Wr(k), Wg(k),Wsl(k),Wsf(k)]
+    [Wof(k+1), Wol(k+1), Wr(k+1), Wg(k+1),Wsl(k+1),Wsf(k+1)]
+    
+    %--
+    NC(k)  = sum(Ncn(k,:));
+    ACS(k) = dot(Ncn(k,:),(1:Ns+1))/NC(k);
+    [a,b] = max(Ncn(k,:).*(Ncn(k,:)>p_thr_sbc));
+    SBC(k) = b;
     
                     
 end
